5553068c728c8cc36e6483f49a24a13456da33cb
Nathan Bashaw <nbashaw@gmail.com>
2011-03-08 17:35:11 -0500

    added menu items

--- public/main.js
+++ public/main.js
@@ -1,5 +1,30 @@
 $(function(){
-  $('input#yes').click(function(){
-    alert('hi');
-  });
+  
+  $("li#blog_link").hover(
+    function () {
+      $('p#blog').fadeIn('fast');
+    }, 
+    function () {
+      $('p#blog').fadeOut('fast');
+    }
+  );
+  
+  $("li#tb_link").hover(
+    function () {
+      $('p#tb').fadeIn('fast');
+    }, 
+    function () {
+      $('p#tb').fadeOut('fast');
+    }
+  );
+  
+  $("li#contact_link").hover(
+    function () {
+      $('p#contact').fadeIn('fast');
+    }, 
+    function () {
+      $('p#contact').fadeOut('fast');
+    }
+  );
+  
 });
