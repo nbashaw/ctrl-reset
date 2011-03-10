01e09623c824fac8d5dd6d2287e8b206673b7d90
Nathan Bashaw <nbashaw@gmail.com>
2011-03-10 11:11:17 -0500

    integrated pony

--- ctrlreset.rb
+++ ctrlreset.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'haml'
 require 'erb'
+require 'pony'
 
 get '/css/:sheet.css' do
   scss params[:sheet].to_sym
@@ -17,6 +18,12 @@ end
 get '/contact' do
   erb :contact
 end
+
+#post '/mail' do
+#  Pony.mail :to => 'nbashaw@gmail.com',
+#            :from => 'me@example.com',
+#            :subject => 'Howdy, Partna!'
+#end  
 
 not_found do
   redirect '/'
